dfe1c0eea8d5c099148707535c419793ff45716e
fabioqcorreia <fabio.correia@supportclik>
2017-04-13 02:08:32 -0300

    conclusao resposta json views.py

--- base/views.py
+++ base/views.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.views.generic import ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import Motorista
 from .forms import MotoristaForm
 from django.http import HttpResponse,JsonResponse
 from django.core import serializers
-import json
 
 
 # Create your views here.
@@ -19,16 +17,17 @@ class CreateMotoristaView(CreateView):
     success_url = reverse_lazy('drivers')
 
 def ListMotoristaView(request):
-    message = None
     template_name = 'Motorista/motorista_list.html'
     status = request.POST.get('data[status]')
 
     if status:
 
-        message = Motorista.objects.filter(status=status)
+        message = serializers.serialize(Motorista.objects.filter("json",status=status))
 
-    return JsonResponse(serializers.serialize('json',message), safe=False)
-
+        return JsonResponse(message, content_type='application/json')
+    else:
+        return HttpResponse('Error')
+    
 class UpdateMotoristaView(UpdateView):
     model = Motorista
     template_name = 'Motorista/motorista_update.html'
