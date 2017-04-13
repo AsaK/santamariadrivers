a96ff74b2803fc9b20bf5e4426dad8e96018893d
fabioqcorreia <fabio.correia@supportclik>
2017-04-12 23:42:54 -0300

    correcao arquivos html sem extensao

--- base/views.py
+++ base/views.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, FormView
 from django.views.generic import ListView
 from django.views import generic
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Motorista
 
+
 # Create your views here.
 
 class CreateMotoristaView(FormView):
-
+    template_name = 'Motorista/motorista_register.html'
 
 class ListMotoristaView(ListView):
     model = Motorista
