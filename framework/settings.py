0107f71c51baf5e5289a41a29dc5d257cfb53fb2
fabioqcorreia <fabio.correia@supportclik>
2017-04-14 09:33:16 -0300

    adicao arquivos api base

--- framework/settings.py
+++ framework/settings.py
@@ -73,6 +73,18 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+ALLOWED_HOSTS = {
+    '192.168.0.35'
+}
+
 WSGI_APPLICATION = 'framework.wsgi.application'
 
 
