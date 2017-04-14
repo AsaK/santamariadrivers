340ac09bc84ac516cc4d6dbf9cfdab5bff19edd5
AsaK <asakbjj@gmail.com>
2017-04-14 10:03:10 -0300

    FIX ERROR!

--- base/views.py
+++ base/views.py
@@ -5,14 +5,13 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 import tempfile
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
 from .serializer import MotoristaSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.shortcuts import render
 from .models import Motorista
 from .forms import MotoristaForm
 
@@ -37,6 +36,7 @@ class MotoristaList(ListView):
             queryset = queryset.filter(status=status)
         return queryset
 
+
 class UpdateMotoristaView(UpdateView):
     model = Motorista
     template_name = 'Motorista/motorista_update.html'
@@ -56,10 +56,3 @@ def deletemotorista(request, idMotorista):
                 return redirect('drivers')
 
     return redirect('drivers')
-
-class MotoristaListView(APIView):
-    serializer_class = MotoristaSerializer
-
-    def get(self,request, format=None):
-        serializer = self.serializer_class(Motorista.objects.all(), many=True)
-        return Response(serializer.data)
