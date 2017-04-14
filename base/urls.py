825d5da91aabccf6241764ffd7bdbae43428e049
AsaK <asakbjj@gmail.com>
2017-04-13 21:20:36 -0300

    Rotas, Motorista!

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import UpdateMotoristaView, deletemotorista, MotoristaList
 
 urlpatterns = [
-    url(r'^list-drivers/', MotoristaList, name='drivers'),
+    url(r'^list-drivers/', MotoristaList.as_view(), name='drivers'),
     url(r'^update-driver/(?P<pk>[0-9]+)', UpdateMotoristaView.as_view(), name='driver-update'),
     url(r'^delete-driver/(?P<pk>[0-9]+)', deletemotorista, name='delete-driver'),
 ]
