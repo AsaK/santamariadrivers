c614a6c9b1b897278512c60663fe11ab48dd56a8
AsaK <asakbjj@gmail.com>
2017-04-13 21:16:07 -0300

    Fixes

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .views import listmotoristaview, UpdateMotoristaView, deletemotorista
+from .views import UpdateMotoristaView, deletemotorista, MotoristaList
 
 urlpatterns = [
-    url(r'^list-drivers/', listmotoristaview, name='drivers'),
+    url(r'^list-drivers/', MotoristaList, name='drivers'),
     url(r'^update-driver/(?P<pk>[0-9]+)', UpdateMotoristaView.as_view(), name='driver-update'),
     url(r'^delete-driver/(?P<pk>[0-9]+)', deletemotorista, name='delete-driver'),
 ]
